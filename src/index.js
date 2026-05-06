25901190dca998af38aec2bb0763d025e66c4626
retro.dev <117005347+ay30bg@users.noreply.github.com>
2026-05-06 10:58:46 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,15 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import { AdminAuthProvider } from './context/AdminAuthContext';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <AdminAuthProvider>
       <App />
-    </AdminAuthProvider>
   </React.StrictMode>
 );
 
